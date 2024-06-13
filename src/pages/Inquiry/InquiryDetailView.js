a541e4379fadd0b94214966f37bbf102b0e40e84
hoje091 <hojeongchoe33@gmail.com>
2024-06-13 19:45:08 +0900

    0613 회원 문의 상세보기 개발중

--- src/pages/Inquiry/InquiryDetailView.js
+++ src/pages/Inquiry/InquiryDetailView.js
@@ -1,11 +1,11 @@
-function InquiryDetailView () {
+function InquiryDetailView ({inquiry : {inquiryCode, inquiryWriteDate, inquiryTitle, inquiryDetail, inquiryReplyStatus}}) {
 
     return (
 
         <>
 
                    문의 코드 <div key={inquiryCode} className="text-center">{inquiryCode}</div>
-                   문의 작성일 <div key={inquiryWriteDate} className="text-center">{inquiryWriteDate}</>
+                   문의 작성일 <div key={inquiryWriteDate} className="text-center">{inquiryWriteDate}</div>
                    문의 제목 <div key={inquiryTitle} className="text-center">{inquiryTitle}</div>
                    문의 내용 <div key={inquiryDetail} className="text-center">{inquiryDetail}</div>
                    문의 처리 상태<div key={inquiryReplyStatus} className="text-center">{inquiryReplyStatus}</div>
